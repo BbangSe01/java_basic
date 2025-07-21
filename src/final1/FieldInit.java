da9933fb6be5853c9f0e790505bdda91dfd0511e
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-07-21 22:51:19 +0900

    final 학습

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10;
+    final int value=10;
+
+    // 이미 final 변수가 초기화돼서 생성자로도 바꿀 수 없음
+//    public FieldInit (int value) {
+//        this.value = value;
+//    }
+}
