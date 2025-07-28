2cbc40421218fbd319546b33a4bf4cb172ead2a5
BbangSe01 <155813689+BbangSe01@users.noreply.github.com>
2025-07-28 19:44:22 +0900

    인터페이스 활용

---
+++ src/Poly/ex5/InterfaceMain.java
@@ -0,0 +1,27 @@
+package Poly.ex5;
+
+import Poly.ex4.AbstractAnimal;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+//        InterfaceAnimal interfaceAnimal = new InterfaceAnimal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+    }
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+    private static void moveAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 이동 테스트 시작");
+        animal.move();
+        System.out.println("동물 이동 테스트 종료");
+    }
+}
